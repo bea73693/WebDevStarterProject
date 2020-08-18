0726d973cf33398ca1e6f1ab0ff78dced76554bc
bea73693 <56211155+bea73693@users.noreply.github.com>
2020-08-18 14:09:59 -0500

    change colors, randomize

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
+@app.route("/flip_color", methods=['POST'])
+def flip_color():
+    json_data = request.get_json()
+    color1 = json_data['color1']
+    color2 = json_data['color2']
+
+    response = {
+        "color1" : color2,
+        "color2" : color1
+    }
+    return response
+
+
+@app.route("/change_color", methods=['POST'])
+def change_color():
+    json_data = request.get_json()
+    return json_data
+
+
 @app.route('/')
 def render():
-    return render_template('base.html')
+    return render_template('table.html')
+
+
